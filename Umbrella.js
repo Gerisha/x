ff3098c267d0db220294127118efac4903020d73
Gerisha <75563368+Gerisha@users.noreply.github.com>
2021-02-08 16:59:06 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,14 @@
+class Umbrella {
+    constructor(x, y, width) {
+      var options = {
+        isStatic: true,
+      };
+      this.body = Bodies.circle(x, y, width - 50, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display() {
+      
+    }
+  }
